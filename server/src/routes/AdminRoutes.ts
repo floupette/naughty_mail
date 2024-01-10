53d4bf9deacb612bf086627ce6ac2e57074c5f70
Naradia <annedraconis@gmail.com>
2024-01-10 15:41:52 +0100

    Début du CRUD

--- server/src/routes/AdminRoutes.ts
+++ server/src/routes/AdminRoutes.ts
@@ -12,3 +12,36 @@ const router = Router();
 router.get('/', async (req, res) => {
     res.send(await Users.find());
 });
+
+router.post('/', async (req,res) => {
+    if(!('firm_name' in req.body)) return res.status(400).send('Missing "firm_name" field');
+
+    const { firm_name } = req.body;
+
+    const user = new Users();
+
+    user.firm_name = firm_name;
+
+    await user.save();
+
+    res.sendStatus(201);
+});
+
+router.get('/:id', async (req, res) => {
+    const user = await Users.findOne({
+        where: { id: Number(req.params.id) }
+    });
+    if(!user) return res.sendStatus(404);
+    res.send(user);
+});
+
+router.delete('/:id', async (req, res) => {
+    const user = await Users.findOne({
+        where: { id: Number(req.params.id) }
+    });
+    if(!user) return res.sendStatus(404);
+    await user.remove();
+    res.sendStatus(200);
+});
+
+export default router;
